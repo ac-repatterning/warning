0935df1b192d004b020d257d8ab751fe6d779f7b
greyhypotheses <10231132+greyhypotheses@users.noreply.github.com>
2026-08-22 14:47:11 +0100

    Update src/compute/interface.py

--- src/compute/interface.py
+++ src/compute/interface.py
@@ -65,7 +65,7 @@ class Interface:
         value: pd.Timestamp = max(data['starting'].min(),
                                   pd.Timestamp(datetime.datetime.now(), tz=zoneinfo.ZoneInfo('Europe/London')))
         starting = self.__timestamp(value = value)
-        ending = self.__timestamp(value = max(data['ending'].max().ceil(freq='h'), value + datetime.timedelta(hours=15)))
+        ending = self.__timestamp(value = data['ending'].max().ceil(freq='18h'))
 
         # Schedule Client
         __schedule_client = self.__connector.client(service_name='scheduler')
